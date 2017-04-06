cab319824e6cc29ce98408f527377baba5719e85
alex <alexander.schnedler@gmail.com>
2017-04-06 13:32:19 -0500

    functional slider

--- slider.js
+++ slider.js
@@ -1,22 +1,28 @@
+
+
+var photoWidth = 500;
+var photoQuantity = 4;
+var photoPosition = -1*photoWidth*(photoQuantity-1);
+
+function manageArrows() {
+  if (photoPosition > photoWidth*-3) {
+    $('.advance-right').show(300);
+  }
+  if(photoPosition === photoWidth*-3) {
+    $('.advance-right').hide(300);
+  }
+  if(photoPosition < 0) {
+    $('.advance-left').show(300);
+  }
+  if(photoPosition === 0) {
+    $('.advance-left').hide(300);
+  }
+}
+
 $(document).ready( function() {
-  var photoWidth=500;
-  var photoQuantity=4;
-  var photoPosition= -1*photoWidth*(photoQuantity-1)
-
-  manageArrows()
   $(".advance-left").click(function() {
-    if(photoPosition<0) {
-      photoPosition+=photoWidth;
-      $('.strip').animate({
-        left: photoPosition+'px'
-      })
-    manageArrows();
-    };
-  });
-
-  $(".advance-right").click(function() {
-    if(photoPosition>-photoWidth*(photoQuantity-1)) {
-      photoPosition-=photoWidth;
+    if(photoPosition < 0) {
+      photoPosition += photoWidth;
       $('.strip').animate({
         left: photoPosition+'px'
       });
@@ -24,23 +30,13 @@ $(document).ready( function() {
     };
   });
 
-  function position() {
-    return Number($('.strip').css("left").slice(0, -2));
-  }
-
-  function manageArrows() {
-    if (photoPosition>photoWidth*-3) {
-      console.log("appear")
-      $('.advance-right').show(300);
+  $(".advance-right").click(function() {
+    if(photoPosition > -photoWidth * (photoQuantity-1)) {
+      photoPosition -= photoWidth;
+      $('.strip').animate({
+        left: photoPosition+'px'
+      });
+    manageArrows();
     }
-    if(photoPosition===photoWidth*-3) {
-      $('.advance-right').hide(300);
-    }
-    if(photoPosition<0) {
-      $('.advance-left').show(300);
-    }
-    if(photoPosition===0) {
-      $('.advance-left').hide(300);
-    }
-  }
+  });
 });
